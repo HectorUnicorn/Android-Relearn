e52aa84b9ea4b8959ae464d2b213f52c32185645
guojialin <guojialin1987@163.com>
2021-08-02 23:16:52 +0800

    添加基础代码

---
+++ app/src/main/java/cc/rememberme/demo/MainActivity.java
@@ -0,0 +1,24 @@
+package cc.rememberme.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.txtHello).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainTabActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+    }
+}
